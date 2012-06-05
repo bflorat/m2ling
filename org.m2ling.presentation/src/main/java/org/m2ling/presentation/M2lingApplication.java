ebf0f5f93a5355086759977fac6d6780cc062428
Bertrand Florat <bertrand@florat.net>
2012-06-05 23:30:30 +0200

    Began Principles GUI

--- org.m2ling.presentation/src/main/java/org/m2ling/presentation/M2lingApplication.java
+++ org.m2ling.presentation/src/main/java/org/m2ling/presentation/M2lingApplication.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.vaadin.Application;
-import com.vaadin.terminal.ThemeResource;
-import com.vaadin.ui.Embedded;
-import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 public class M2lingApplication extends Application {
@@ -15,35 +14,40 @@ public class M2lingApplication extends Application {
 
 	private Window mainWindow;
 
+	private MainFrame mframe;
+
+	private MenuBar menu;
+
 	/**
 	 * List of available applications. TODO : call a service to get available applications for
-	 * currently loged-in user.
+	 * currently logged-in user.
 	 **/
-	private List<String> availableApps = Lists.asList("m2principles",null);
-	 
+	private List<String> availableApps = Lists.newArrayList("Principles", "Configuration");
+
 	@Override
 	public void init() {
 		mainWindow = new Window("m2ling");
 		setTheme("m2ling_theme");
 		setMainWindow(mainWindow);
-		initAppsSelector();
+		// Use all the screen size
+		mainWindow.getContent().setSizeFull();
+		initMenu();
+		mframe = new MainFrame();
+		VerticalLayout rootLayout = (VerticalLayout) mainWindow.getContent();
+		mainWindow.addComponent(menu);
+		mainWindow.addComponent(mframe);
+		rootLayout.setSpacing(true);
+		rootLayout.setExpandRatio(mframe, 100);
 	}
 
-	private void initAppsSelector() {
-		TabSheet tabs = new TabSheet();
-		tabs.setHeight("132px");
-		tabs.setWidth("100%");
-
-		// logo
-		ThemeResource resLogo = new ThemeResource("img/m2ling_logo.png");
-		Embedded logo = new Embedded(null, resLogo);
-
-		// Build the final tab sheet and adds it to the main window
-		tabs.addTab(logo).setStyleName("logo");
-		for (String app : availableApps){
-			TabSheet tabsApp = new TabSheet();
-			tabs.addTab(tabsApp,app).setStyleName("app");
+	private void initMenu() {
+		menu = new MenuBar();
+		menu.setWidth("100%");
+		menu.setHeight("30px");
+		for (String app : availableApps) {
+			menu.addItem(app, null, null);
 		}
-		mainWindow.addComponent(tabs);
+		menu.setStyleName("app");
 	}
+
 }
